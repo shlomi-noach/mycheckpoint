a2fed36ae7f342caed060a2b8cdf9ae2b20f4ce3
shlomi.noach <shlomi.noach@fc422328-be13-11de-8784-f7ad4773dbab>
2009-10-25 18:01:42 +0000

    Added debian + python distribution scripts

---
+++ py-dist-cfg/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name="mycheckpoint",
+    description="Monitoring for MySQL",
+    author="Shlomi Noach",
+    author_email="shlomi@code.openark.org",
+    url="http://code.openark.org/forge/mycheckpoint",
+    version="10",
+    requires=["MySQLdb"],
+    packages=[""],
+    package_dir={"": "scripts"},
+    scripts=[
+        "mycheckpoint",
+        ]
+)
