9ad7d6e7a5953259e52769879900cd0765af7b05
shlomi.noach <shlomi.noach@fc422328-be13-11de-8784-f7ad4773dbab>
2009-12-16 16:22:09 +0000

    Revision for release

--- py-dist-cfg/setup.py
+++ py-dist-cfg/setup.py
@@ -6,7 +6,7 @@ setup(
     author="Shlomi Noach",
     author_email="shlomi@code.openark.org",
     url="http://code.openark.org/forge/mycheckpoint",
-    version="54",
+    version="57",
     requires=["MySQLdb"],
     packages=[""],
     package_dir={"": "scripts"},
