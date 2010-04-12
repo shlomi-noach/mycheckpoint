b886bb58593fc13a0ac69dff5dfbd95db434aae9
shlomi.noach@gmail.com <shlomi.noach@gmail.com@fc422328-be13-11de-8784-f7ad4773dbab>
2010-04-12 17:00:30 +0000

    Fix to sv_diff: when either one of the parameters is NULL, the diff value is now NULL. Minor change to flattened custom charts.

--- src/mycheckpoint.py
+++ src/mycheckpoint.py
@@ -1467,7 +1467,9 @@ def create_status_variables_diff_view():
     status_columns_listing = ",\n".join([" ${status_variables_table_alias}2.%s AS %s" % (column_name, column_name,) for column_name in status_columns])
     # Status variables are diffed. This does not make sense for all of them, but we do it for all nonetheless.
     diff_signed_columns_listing = ",\n".join([" ${status_variables_table_alias}2.%s - ${status_variables_table_alias}1.%s AS %s_diff" % (column_name, column_name, column_name, ) for column_name in status_columns if is_signed_column(column_name)])
-    diff_unsigned_columns_listing = ",\n".join([" IF(${status_variables_table_alias}2.%s >= ${status_variables_table_alias}1.%s , ${status_variables_table_alias}2.%s - ${status_variables_table_alias}1.%s, ${status_variables_table_alias}2.%s) AS %s_diff" % (column_name, column_name, column_name, column_name, column_name, column_name, ) for column_name in status_columns if not is_signed_column(column_name)])
+    # When either sv1's or sv2's variable is NULL, the IF condition fails and we do the "-" math, leading again to NULL. 
+    # I *want* the diff to be NULL. This makes more sense than choosing sv2's value.
+    diff_unsigned_columns_listing = ",\n".join([" IF(${status_variables_table_alias}2.%s < ${status_variables_table_alias}1.%s, ${status_variables_table_alias}2.%s) AS %s_diff, ${status_variables_table_alias}2.%s - ${status_variables_table_alias}1.%s" % (column_name, column_name, column_name, column_name, column_name, column_name, ) for column_name in status_columns if not is_signed_column(column_name)])
 
     query = """
         CREATE
@@ -2356,7 +2358,7 @@ def create_custom_google_charts_flattened_views():
     for i in range(num_custom_columns):
         custom_clause = """
             MAX(IF(custom_query_id=%d AND enabled, chart, NULL)) AS custom_%d_chart,
-            MAX(IF(custom_query_id=%d, description, NULL)) AS custom_%d_description
+            MAX(IF(custom_query_id=%d, CONCAT(description, ' [', chart_type, ']'), NULL)) AS custom_%d_description
             """ % (i, i, i, i,)
         custom_clauses.append(custom_clause)
         
