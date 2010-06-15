a7e96537119b7735cdc1b9c093ce843020390864
shlomi.noach@gmail.com <shlomi.noach@gmail.com@fc422328-be13-11de-8784-f7ad4773dbab>
2010-06-15 09:11:27 +0000

    Initial work on detecting swap activity. This may not make it into production. metadata table now stores ids and order of custom queries. auto deply affected by above change: change in number or order of custom queries leads to auto deply. google 24/7 charts now use gradient Removed dygraphs, graphael from code and into scrap pages Better presentation of custom queries: - Now legend includes description on custom HTML brief page - Now ordering them by chart_order on custom HTML brief page - Now only presenting existing custom queries on custom HTML brief page
    Attempt at self created javascript charts, implemented in create_report_html_brief_interactive_view. this will take some more time to develop

--- src/mycheckpoint.py
+++ src/mycheckpoint.py
@@ -442,6 +442,39 @@ def get_mountpoint_usage_percent(path):
     return used_percent
 
 
+def get_swap_activity():
+    """
+    """
+    # First, detect swap mount point. Actually, there can be many.
+    swaps_file = open("/proc/swaps")
+    swap_1st_mount_line = swaps_file.readlines()[1]
+    swaps_file.close()
+    
+    swap_mount = swap_1st_mount_line.split()[0]
+    swap_mount = swap_mount.split("/")[-1]
+    
+    # We now look for disk activity for this device:
+    f = open("/proc/diskstats")
+    lines = f.readlines()
+    f.close()
+    
+    sectors_read = None
+    sectors_written = None
+    
+    for line in lines:
+        tokens = line.split()
+        if tokens[2] == swap_mount:
+            tokens = tokens[3:]
+            if len(tokens) == 4:
+                # This is on 2.6 kernel
+                sectors_read = int(tokens[1])
+                sectors_written = int(tokens[3])
+            elif len(tokens) == 11:
+                sectors_read = int(tokens[2])
+                sectors_written = int(tokens[6])
+    return (sectors_read, sectors_written)
+
+
 def get_custom_query_ids():
     """
     Returns the ordered ids
@@ -838,8 +871,14 @@ def create_custom_query_table():
             traceback.print_exc()
         exit_with_error("Cannot create table %s.custom_query" % database_name)
     
-    query = """UPDATE metadata SET custom_queries = (SELECT GROUP_CONCAT(custom_query_id ORDER BY chart_order, custom_query_id SEPARATOR ',') FROM custom_query) 
+    query = """UPDATE ${database_name}.metadata 
+        SET custom_queries = 
+            (SELECT 
+                GROUP_CONCAT(custom_query_id ORDER BY chart_order, custom_query_id SEPARATOR ',') 
+            FROM ${database_name}.custom_query
+            ) 
     """
+    query = query.replace("${database_name}", database_name)
     act_query(query)
 
 
@@ -1477,17 +1516,19 @@ def get_monitored_host_mysql_version():
 def is_same_deploy():
     try:
         query = """SELECT COUNT(*) AS same_deploy 
-            FROM %s.metadata 
+            FROM ${database_name}.metadata 
             WHERE 
               revision = %d 
               AND build = %d 
               AND mysql_version = '%s'
-              AND custom_queries = (SELECT GROUP_CONCAT(custom_query_id ORDER BY chart_order, custom_query_id SEPARATOR ',') FROM custom_query)
-              """ % (database_name, revision_number, build_number, get_monitored_host_mysql_version())
+              AND custom_queries = (SELECT GROUP_CONCAT(custom_query_id ORDER BY chart_order, custom_query_id SEPARATOR ',') FROM ${database_name}.custom_query)
+              """ % (revision_number, build_number, get_monitored_host_mysql_version())
+        query = query.replace("${database_name}", database_name)
         same_deploy = get_row(query, write_conn)["same_deploy"]
         return (same_deploy > 0)
     except:
         return False
+
 
 def column_name_relates_to_custom_query(column_name):
     if re.match("^custom_[\\d]+$", column_name):
@@ -1816,9 +1857,8 @@ def create_report_24_7_view():
 
 
 def generate_google_chart_24_7_query(chart_column):
-
-    chart_color = "4682b4"
-    chart_color = "7ec97e,e6e862,c45b5b"
+    # Gradient color:
+    chart_color = "9aed32,ff8c00"
 
     query = """
           REPLACE(
@@ -2369,38 +2409,6 @@ def create_report_google_chart_views(charts_list):
     verbose("report charts views created")
 
 
-
-def create_report_dygraph_chart_views(charts_list):
-    for view_name_extension in ["sample", "hour", "day"]:
-        charts_queries = []
-        for (chart_columns, alias, _scale_from_0, _scale_to_100) in charts_list:
-            non_breakable_chart_columns = chart_columns.replace(" ", "")
-            chart_columns_list = chart_columns.split(",")
-            chart_columns_query_clause = ", ',', ".join(["IFNULL(ROUND(%s, 2), '')" % chart_column for chart_column in chart_columns_list])
-            charts_queries.append(
-                "CONCAT('Date,%s\\\\n', GROUP_CONCAT(CONCAT(timeseries_ts, ',', %s, '\\\\n') ORDER BY timeseries_ts SEPARATOR '')) AS %s" % (non_breakable_chart_columns, chart_columns_query_clause, alias)
-            )
-            pass
-        charts_query = ",".join(charts_queries)
-        query = """
-            CREATE
-            OR REPLACE
-            ALGORITHM = TEMPTABLE
-            DEFINER = CURRENT_USER
-            SQL SECURITY INVOKER
-            VIEW ${database_name}.sv_report_dygraph_${view_name_extension} AS
-              SELECT
-                %s
-              FROM
-                ${database_name}.sv_report_chart_${view_name_extension}_timeseries
-            """ % charts_query
-        query = query.replace("${database_name}", database_name)
-        query = query.replace("${view_name_extension}", view_name_extension)
-        act_query(query)
-
-    verbose("dycharts views created")
-
-
 def create_custom_google_charts_views():
     chart_type_extensions = {
             "value": "",
@@ -2445,8 +2453,9 @@ def create_custom_google_charts_flattened_views():
     for i in get_custom_query_ids():
         custom_clause = """
             MAX(IF(custom_query_id=%d AND enabled, chart, NULL)) AS custom_%d_chart,
+            MAX(IF(custom_query_id=%d, description, NULL)) AS custom_%d_text_description,
             MAX(IF(custom_query_id=%d, CONCAT(description, ' [', chart_type, ']'), NULL)) AS custom_%d_description
-            """ % (i, i, i, i,)
+            """ % (i, i, i, i, i, i,)
         custom_clauses.append(custom_clause)
         
     for view_name_extension in ["sample", "hour", "day"]:
@@ -2683,35 +2692,29 @@ def create_report_html_brief_view(report_charts):
     verbose("sv_report_html_brief created")
 
 
+
 def create_report_html_brief_interactive_view(report_charts):
     charts_sections_list = [chart_section for (chart_section, charts_aliases) in report_charts]
     chart_aliases_navigation_map = " | ".join(["""<a href="#%s">%s</a>""" % (chart_section, chart_section) for chart_section in charts_sections_list if chart_section])
 
     sections_queries = []
+    js_queries = []
     for (chart_section, charts_aliases) in report_charts:
         charts_aliases_list = [chart_alias.strip() for chart_alias in charts_aliases.split(",")]
         charts_aliases_queries = []
         for chart_alias in charts_aliases_list:
-            query = """
-                '<div id = "chartDiv_${chart_alias}" class="chart">
-                  <div class="controls"><button onClick="g_${chart_alias}.toggleMaximize()">+/-</button><h4>%s</h4></div>
-                  <div id="graphDiv_${chart_alias}" class="graphdiv" style="width:', charts_api.chart_width, ';height:', charts_api.chart_height-40, '"></div>
-                  <div id="labelsDiv_${chart_alias}" class="legend"></div>
-                </div>
-                <script type="text/javascript">
-                    g_${chart_alias} = new Dygraph(
-                        document.getElementById("graphDiv_${chart_alias}"),
-                        "', IFNULL(${chart_alias}, ''), '",
-                        {
-                            chartDiv:  document.getElementById("chartDiv_${chart_alias}"),
-                            labelsDiv: document.getElementById("labelsDiv_${chart_alias}")
-                        }
-                    );
-                </script>',
-                """ % (chart_alias.replace("_", " "),)
-            query = query.replace("${chart_alias}", chart_alias)
-            charts_aliases_queries.append(query)
+            div_query = """'<div id="chart_div_${chart_alias}" class="chart"></div>',
+                """
+            div_query = div_query.replace("${chart_alias}", chart_alias)
+            charts_aliases_queries.append(div_query)
+
+            js_query = """'
+                    new openark_msline(document.getElementById("chart_div_${chart_alias}"), {width: ', chart_width, ', height: ',  chart_height-20, '}).read_google_url("', ${chart_alias}, '");
+                '""" 
+            js_query = js_query.replace("${chart_alias}", chart_alias)
+            js_queries.append(js_query)
         charts_aliases_query = "".join(charts_aliases_queries)
+        js_query = ",".join(js_queries)
         
         chart_section_anchor = chart_section 
         if not chart_section:
@@ -2738,9 +2741,13 @@ def create_report_html_brief_interactive_view(report_charts):
           SELECT CONCAT('
             <html>
                 <head>
-                    <script type="text/javascript" src="mycheckpoint/build/dygraphs/dygraph-combined.js"></script>
                     <title>mycheckpoint brief report</title>
                     <meta http-equiv="refresh" content="600" />
+                    <script type="text/javascript" charset="utf-8">
+                        window.onload = function () {
+                    ', %s, '
+                        };
+                    </script>
                     <style type="text/css">
                     body {
                         color:#505050;
@@ -2755,13 +2762,11 @@ def create_report_html_brief_interactive_view(report_charts):
                         white-space: nowrap;
                         margin-right: 10px;
                         font-family: Helvetica,Verdana,Arial,sans-serif;
+                        width: ', chart_width, 'px;
                     }
                     .clear {
                         clear:both;
                         height:1px;
-                    }
-                    div.chart {
-                        float: left;
                     }
                     a {
                         color:#f26522;
@@ -2781,29 +2786,6 @@ def create_report_html_brief_interactive_view(report_charts):
                         margin-bottom: 0px;
                         padding-left: 8px;
                     }
-                    .controls {
-                        padding-left: 44px;
-                        margin-bottom: 4px;
-                    }
-                    .controls button {
-                        display: inline;
-                    }
-                    ul {
-                        font-size:9pt;
-                        margin: 0px;
-                        padding-left: 44px;
-                    }
-                    li:first-child { 
-                        list-style-type: none; 
-                        font-weight: bold;
-                    }
-                    li { 
-                        list-style-type: square; 
-                    }
-                    li div { 
-                        color: #404040;
-                        font-size:9pt;
-                    }
                     </style>
                 </head>
                 <body>
@@ -2819,8 +2801,8 @@ def create_report_html_brief_interactive_view(report_charts):
             </html>
           ') AS html
           FROM
-            ${database_name}.sv_report_dygraph_sample, ${database_name}.charts_api
-        """ % "".join(sections_queries)
+            ${database_name}.sv_report_chart_sample, ${database_name}.charts_api
+        """ % (js_query, "".join(sections_queries))
     query = query.replace("${database_name}", database_name)
     query = query.replace("${chart_aliases_navigation_map}", chart_aliases_navigation_map)
     query = query.replace("${global_width}", str(options.chart_width*3 + 30))
@@ -2934,10 +2916,10 @@ def create_custom_html_brief_view():
             '<div class="chart">
                 <h3>', IFNULL(custom_%d_description, 'N/A'), '</h3>',
                 IFNULL(
-                  CONCAT('<img src="', custom_%d_chart, '"/>'), 
+                  CONCAT('<img src="', REPLACE(custom_%d_chart, '&chdl=custom_', CONCAT('&chdl=', IFNULL(CONCAT(custom_%d_text_description, ':+'), ''), 'custom_')), '"/>'), 
                   '<div class="img_dummy">Chart N/A</div>'),
             '</div>',
-            """ % (i, i)
+            """ % (i, i, i)
         custom_charts_queries.append(custom_chart_query)
         
     query = """
@@ -3358,7 +3340,6 @@ def create_status_variables_views():
     report_chart_views.extend([
         (custom_variable, custom_variable, True, False) for custom_variable in get_custom_status_variables_psec()
         ])
-    create_report_dygraph_chart_views(report_chart_views)
     create_report_google_chart_views(report_chart_views)
     report_24_7_columns = [
         "innodb_read_hit_percent",
@@ -3427,6 +3408,7 @@ def create_status_variables_views():
             ("", "os_cpu_utilization_percent, os_loadavg, os_mountpoints_usage_percent"),
         ]
     create_report_html_brief_view(brief_html_view_charts)
+    #create_report_html_brief_interactive_view(brief_html_view_charts)
     create_custom_html_view()
     create_custom_html_brief_view()
     #create_report_html_brief_interactive_view(brief_html_view_charts)
